18a674ae01ffa0adec4f01693f120d906e45737f
Jhody Pedia <53709496+4rsl3G@users.noreply.github.com>
2025-12-29 04:04:37 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const expressLayouts = require('express-ejs-layouts'); // Import Lib
 const routes = require('./src/routes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Setup View Engine
+// Setup View Engine & Layouts
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
-// Middleware
+// Konfigurasi EJS Layouts
+app.use(expressLayouts);
+app.set('layout', 'layout'); // Mengarah ke src/views/layout.ejs
+app.set('layout extractScripts', true); // Ekstrak script dari page ke bawah layout
+app.set('layout extractStyles', true);  // Ekstrak style dari page ke head layout
+
+// Middleware Standard
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Global Middleware for Language
+// Global Middleware
 app.use((req, res, next) => {
     res.locals.lang = req.query.lang || req.cookies.lang || 'en';
-    // Helper to keep query params
     res.locals.currentUrl = req.originalUrl.split('?')[0];
     next();
 });
